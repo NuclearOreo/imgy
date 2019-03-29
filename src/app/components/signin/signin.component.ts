d47b26d3edf6be7495b84903daf502945033f66f
Ussama Azam <nuclearoreo@gmail.com>
2019-03-28 20:31:22 -0400

    finished signin components

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ImgyApiService } from 'src/app/imgy-api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
+
 export class SigninComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor(private service: ImgyApiService) { }
+  constructor(private service: ImgyApiService, private router: Router) { }
 
   ngOnInit() {
   }
 
   signIn() {
     this.service.geToken(this.email, this.password).subscribe(
-      (res) => {
-        console.log(res);
+      (res: {token: string}) => {
+        localStorage.setItem('x-auth-token', res.token);
+        this.router.navigateByUrl('/');
       }
     );
   }
