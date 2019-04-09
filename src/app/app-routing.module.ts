2890492155cce4bcfb56a3ecca1b3cc71e9d2ce2
Ussama Azam <nuclearoreo@gmail.com>
2019-04-08 20:18:18 -0400

    setup route to view post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { SignupComponent } from './components/signup/signup.component';
 import { HidepagesService } from './service/hidepages.service';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuardService } from './service/auth-guard.service';
+import { PostComponent } from './components/post/post.component';
 
 const routes: Routes = [
   { path: '', component: GalleryComponent },
   { path: 'Profile', component: ProfileComponent, canActivate: [AuthGuardService] },
+  { path: 'Post/:id', component: PostComponent },
   { path: 'SignIn', component: SigninComponent, canActivate: [HidepagesService] },
   { path: 'SignUp', component: SignupComponent, canActivate: [HidepagesService] },
   { path: '**', component: GalleryComponent },
