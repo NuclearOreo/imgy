d8386aa8b8a550c7a0eea0f27c87487f464d0ed7
Ussama Azam <nuclearoreo@gmail.com>
2019-04-09 19:56:31 -0400

    have html setup for component

--- src/app/components/mypost/mypost.component.ts
+++ src/app/components/mypost/mypost.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ImgyApiService } from 'src/app/imgy-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from './post';
 
 @Component({
   selector: 'app-mypost',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mypost.component.css']
 })
 export class MypostComponent implements OnInit {
+  post: Post;
+  comments = [];
 
-  constructor() { }
+  constructor(private service: ImgyApiService, private route: ActivatedRoute) {
+    const id = this.route.snapshot.paramMap.get('id');
+    service.getPostById(id).subscribe((post: Post) => {
+      this.post = post; this.getComments();
+    });
+  }
 
   ngOnInit() {
   }
+
+  async getComments() {
+    // tslint:disable-next-line:prefer-const
+    for (let commentsId of this.post.comments) {
+      const comment = await this.service.getCommentsbyId(commentsId).toPromise();
+      this.comments.push(comment);
+    }
+ }
 
 }
---
+++ src/app/components/mypost/post.ts
@@ -0,0 +1,6 @@
+export interface Post {
+    _id: string;
+    username: string;
+    comments: [string];
+    imageUrl: string;
+}
