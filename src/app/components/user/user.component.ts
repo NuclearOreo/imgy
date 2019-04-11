eb0362670503d20f892ffafc099a8f235994f47f
Ussama Azam <nuclearoreo@gmail.com>
2019-04-11 17:24:01 -0400

    Can now view someone's profile

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ImgyApiService } from 'src/app/imgy-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -6,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  list: object;
+  info: object;
+  comments: object;
 
-  constructor() { }
+  constructor(private service: ImgyApiService, private route: ActivatedRoute) {
+    const username = route.snapshot.paramMap.get('username');
+    this.service.getPost(username).subscribe(
+      (res) => { this.list = res; },
+      (err) => { console.log(err); });
+
+    this.service.getProfile(username).subscribe(
+      (res) => { this.info = res; },
+      (err) => { console.log(err); });
+
+    this.service.getCommentsbyUsername(username).subscribe(
+      (res) => { this.comments = res; }
+    );
+   }
 
   ngOnInit() {
   }
