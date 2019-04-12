85d74096a8c4ea6a8beb001ca0e38c01ba14d67e
Ussama Azam <nuclearoreo@gmail.com>
2019-04-12 14:25:21 -0400

    Made the auth guard better

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -7,11 +7,17 @@ import { ImgyApiService } from '../imgy-api.service';
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: ImgyApiService) { }
 
   async canActivate() {
     const token = localStorage.getItem('x-auth-token');
     if (token) {
+      await this.service.verify(token).toPromise().catch(
+        (err) => {
+          localStorage.removeItem('x-auth-token');
+          this.router.navigateByUrl('/');
+          return false;
+        });
       return true;
     }
     this.router.navigateByUrl('/');
